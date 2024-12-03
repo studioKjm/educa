60717354e77c2a865217d3c62b07ef72c6da11f8
jimin Kim <117446950+studioKjm@users.noreply.github.com>
2024-12-03 14:50:48 +0900

    12.3.4

--- educa/settings/base.py
+++ educa/settings/base.py
@@ -15,7 +15,7 @@ from pathlib import Path
 from django.conf.global_settings import MEDIA_URL
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
